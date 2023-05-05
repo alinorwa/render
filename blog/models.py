35d8e2f898d590df86c05175435e791c12c373bb
alinorwa <alialrubay399@gmail.com>
2023-05-05 09:35:48 -0700

    img

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,18 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    
 
     def __str__(self) -> str:
         return self.title
+    
+
+class Image(models.Model):
+   
+    image = models.ImageField(upload_to='images/')
+    
+
+   
+    
+
+
--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import dj_database_url
 import environ
@@ -134,11 +135,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+
+# STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",]
 STATICFILES_DIRS = [
-    BASE_DIR / "static",]
+    os.path.join(BASE_DIR, 'project', 'static'),
+]
 
 MEDIA_ROOT = 'media/'
+
 MEDIA_URL = '/media/'
 
 
